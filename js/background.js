18c13c4ae5f2b875a430a4de310111515c4dad1e
Yang. Han Jun <71217361+97970z@users.noreply.github.com>
2022-03-13 19:56:29 +0900

    Renew background imgae

--- js/background.js
+++ js/background.js
@@ -1,5 +1,5 @@
 
-const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg"];
+const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"];
 
 const awesomeImages = images[Math.floor(Math.random() * images.length)];
 
